b60619d5c3252e4b9f0e94ddc02e43bfccb3a0d5
JunYoung <twinjyjh2@gmail.com>
2021-11-10 14:47:40 +0900

    21.11.10 Redux Study content-01

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const add = document.querySelector(".add");
+const minus = document.querySelector(".minus");
+const number = document.querySelector(".number");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const reducer = (count = 0, action) => {  // reducer는 함수의 형태로 만들어야 함
+  console.log(count);
+  switch (action.type) {
+    case "ADD":
+      return count += 1;
+    case "MINUS":
+      return count -= 1;
+    default:
+      return count;
+  }
+}
+
+const store = createStore(reducer);
+
+const onChange = () => {
+  number.innerText = store.getState()
+}
+
+store.subscribe(onChange);
+
+add.addEventListener('click', () => store.dispatch({type: "ADD"}))
+minus.addEventListener('click', () => store.dispatch({type: "MINUS"}))
